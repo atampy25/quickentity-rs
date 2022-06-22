7885d3f610c0aacf330e4fd387a2095b4138465f
Atampy26 <24306974+hitman-resources@users.noreply.github.com>
2022-06-22 19:08:55 +1000

    RT structs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-mod structs;
+mod qn_structs;
+mod rt_structs;
 
-use std::{fs, io::Read};
+use qn_structs::Entity;
+use rt_structs::{PropertyID, RTBlueprint, RTTemplate, STemplateFactorySubEntity};
 
 use serde_json::{from_slice, to_string, Value};
-use structs::Entity;
+use std::{fs, io::Read};
 
 fn read_as_json(path: &String) -> Value {
     from_slice(&{
@@ -29,11 +31,53 @@ fn read_as_entity(path: &String) -> Entity {
     .expect("Failed to open file as JSON")
 }
 
-fn main() {
-    let entity: Entity = read_as_entity(&String::from("entity.json"));
+fn read_as_rttemplate(path: &String) -> RTTemplate {
+    from_slice(&{
+        let mut vec = Vec::new();
+        fs::File::open(path)
+            .expect("Failed to open file")
+            .read_to_end(&mut vec)
+            .expect("Failed to read file");
+        vec
+    })
+    .expect("Failed to open file as JSON")
+}
 
-    for (entity_id, entity_data) in entity.entities.iter() {
-        println!("{}", entity_id);
-        println!("{:?}", entity_data);
-    }
+fn read_as_rtblueprint(path: &String) -> RTBlueprint {
+    from_slice(&{
+        let mut vec = Vec::new();
+        fs::File::open(path)
+            .expect("Failed to open file")
+            .read_to_end(&mut vec)
+            .expect("Failed to read file");
+        vec
+    })
+    .expect("Failed to open file as JSON")
+}
+
+fn main() {
+    let entity = read_as_rtblueprint(&String::from("entity.TBLU.json"));
+
+    // let chars = fs::read_to_string(&String::from("entity.TEMP.json"))
+    //     .unwrap()
+    //     .chars()
+    //     .collect::<Vec<char>>()[23850..23900]
+    //     .to_vec()
+    //     .iter()
+    //     .collect::<String>();
+
+    // dbg!(chars);
+
+    // dbg!(entity
+    //     .sub_entities
+    //     .iter()
+    //     .filter(
+    //         |x| x.property_values.iter().any(|y| match &y.n_property_id {
+    //             PropertyID::String(val) => val == "m_fValue",
+    //             _ => false,
+    //         })
+    //     )
+    //     .collect::<Vec<&STemplateFactorySubEntity>>());
+
+    dbg!(entity);
 }
---
+++ src/rt_structs.rs
@@ -0,0 +1,145 @@
+use serde::{Deserialize, Serialize};
+use serde_json;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct STemplateBlueprintSubEntity {
+    pub logical_parent: SEntityTemplateReference,
+    pub entity_type_resource_index: i32,
+
+    #[serde(rename = "entityId")]
+    pub entity_id: u64,
+
+    pub editor_only: bool,
+    pub entity_name: String,
+    pub property_aliases: Vec<SEntityTemplatePropertyAlias>,
+    pub exposed_entities: Vec<SEntityTemplateExposedEntity>,
+    pub exposed_interfaces: Vec<(String, i32)>,
+    pub entity_subsets: Vec<(String, SEntityTemplateEntitySubset)>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct RTBlueprint {
+    pub sub_type: i32,
+    pub root_entity_index: i32,
+    pub sub_entities: Vec<STemplateBlueprintSubEntity>,
+    pub external_scene_type_indices_in_resource_header: Vec<i32>,
+    pub pin_connections: Vec<SEntityTemplatePinConnection>,
+    pub input_pin_forwardings: Vec<SEntityTemplatePinConnection>,
+    pub output_pin_forwardings: Vec<SEntityTemplatePinConnection>,
+    pub override_deletes: Vec<SEntityTemplateReference>,
+    pub pin_connection_overrides: Vec<SExternalEntityTemplatePinConnection>,
+    pub pin_connection_override_deletes: Vec<SExternalEntityTemplatePinConnection>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct STemplateFactorySubEntity {
+    pub logical_parent: SEntityTemplateReference,
+    pub entity_type_resource_index: i32,
+    pub property_values: Vec<SEntityTemplateProperty>,
+    pub post_init_property_values: Vec<SEntityTemplateProperty>,
+    pub platform_specific_property_values: Vec<SEntityTemplatePlatformSpecificProperty>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct RTTemplate {
+    pub sub_type: i32,
+    pub blueprint_index_in_resource_header: i32,
+    pub root_entity_index: i32,
+    pub sub_entities: Vec<STemplateFactorySubEntity>,
+    pub property_overrides: Vec<SEntityTemplatePropertyOverride>,
+    pub external_scene_type_indices_in_resource_header: Vec<i32>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SEntityTemplateReference {
+    #[serde(rename = "entityID")]
+    pub entity_id: u64,
+
+    pub external_scene_index: i32,
+    pub entity_index: i32,
+    pub exposed_entity: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SEntityTemplateExposedEntity {
+    pub s_name: String,
+    pub b_is_array: bool,
+    pub a_targets: Vec<SEntityTemplateReference>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SEntityTemplatePinConnection {
+    #[serde(rename = "fromID")]
+    pub from_id: i32,
+
+    #[serde(rename = "toID")]
+    pub to_id: i32,
+
+    pub from_pin_name: String,
+    pub to_pin_name: String,
+    pub constant_pin_value: serde_json::Value,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SEntityTemplatePlatformSpecificProperty {
+    pub property_value: SEntityTemplateProperty,
+    pub platform: String,
+    pub post_init: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SEntityTemplatePropertyAlias {
+    pub s_alias_name: String,
+
+    #[serde(rename = "entityID")]
+    pub entity_id: i32,
+
+    pub s_property_name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SEntityTemplatePropertyOverride {
+    pub property_owner: SEntityTemplateReference,
+    pub property_value: SEntityTemplateProperty,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SEntityTemplateEntitySubset {
+    pub entities: Vec<i32>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SExternalEntityTemplatePinConnection {
+    pub from_entity: SEntityTemplateReference,
+    pub to_entity: SEntityTemplateReference,
+    pub from_pin_name: String,
+    pub to_pin_name: String,
+    pub constant_pin_value: serde_json::Value,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SEntityTemplateProperty {
+    #[serde(rename = "nPropertyID")]
+    pub n_property_id: PropertyID,
+
+    pub value: serde_json::Value,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum PropertyID {
+    Int(u64),
+    String(String),
+}
