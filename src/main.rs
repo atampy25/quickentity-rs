a3ada8c7e47d835d551f389d7c9c5f27bd84fcb7
atampy25 <24306974+atampy25@users.noreply.github.com>
2026-06-21 22:59:34 +1000

    Fix CLI

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ use std::fs;
 use quickentity_rs::{apply_patch, entity::Entity, generate_patch, patch::Patch};
 
 use anyhow::Result;
-use clap::{Parser, Subcommand};
-use glacier_bin1::game::conversion::ConvertFrom;
-use glacier_commons::game::GameVersion;
+use clap::{Parser, Subcommand, ValueEnum};
 use serde_json::from_slice;
 use tryvial::try_fn;
 
@@ -18,6 +16,21 @@ use crate::io_utils::{read_as_json, to_vec_float_format};
 struct Args {
 	#[command(subcommand)]
 	command: Command
+}
+
+#[derive(ValueEnum, Clone, Copy)]
+enum GameVersion {
+	#[cfg(feature = "h1")]
+	H1,
+
+	#[cfg(feature = "h2")]
+	H2,
+
+	#[cfg(feature = "h3")]
+	H3,
+
+	#[cfg(feature = "fl")]
+	FL
 }
 
 #[derive(Subcommand)]
@@ -48,6 +61,10 @@ enum Command {
 enum EntityCommand {
 	/// Convert a set of JSON files into a QuickEntity JSON file.
 	Convert {
+		/// Game version.
+		#[arg(short = 'v', long)]
+		version: GameVersion,
+
 		/// Factory (TEMP) JSON path.
 		#[arg(short = 'i', long)]
 		input_factory: String,
@@ -79,6 +96,10 @@ enum EntityCommand {
 		#[arg(short = 'i', long)]
 		input: String,
 
+		/// Game version.
+		#[arg(short = 'v', long)]
+		version: GameVersion,
+
 		/// Factory (TEMP) JSON path.
 		#[arg(short = 'o', long)]
 		output_factory: String,
@@ -93,26 +114,7 @@ enum EntityCommand {
 
 		/// Blueprint (TBLU) meta JSON path.
 		#[arg(short = 'r', long)]
-		output_blueprint_meta: String,
-
-		/// Output ResourceLib JSON files compatible with HITMAN (2016).
-		#[arg(long, action)]
-		h1: bool
-	},
-
-	/// Output the same QuickEntity JSON in standard form, including consistent entity ID lengths and sorted JSON keys.
-	Normalise {
-		/// Input QuickEntity JSON path.
-		#[arg(short = 'i', long)]
-		input: String,
-
-		/// Output QuickEntity JSON path.
-		#[arg(short = 'o', long)]
-		output: String,
-
-		/// Convert keeping all scale values, no matter if insignificant (1.00 when rounded to 2 d.p.).
-		#[arg(short = 's', long, action)]
-		lossless: bool
+		output_blueprint_meta: String
 	}
 }
 
@@ -155,10 +157,6 @@ enum PatchCommand {
 		#[arg(long, action)]
 		permissive: bool,
 
-		/// Ensure the resulting QuickEntity JSON is valid and output the JSON in standard form, including consistent entity ID lengths and sorted JSON keys.
-		#[arg(long, action)]
-		normalise: bool,
-
 		/// Mitigate a serde-json issue where numbers are sometimes not considered equal by parsing JSON files twice.
 		#[arg(long, action)]
 		format_fix: bool
@@ -185,16 +183,47 @@ fn main() -> Result<()> {
 					input_blueprint,
 					input_blueprint_meta,
 					output,
-					lossless
+					lossless,
+					version
 				}
 		} => {
-			let entity = Entity::from_game(
-				&read_as_json(input_factory),
-				&read_as_json(input_factory_meta),
-				&read_as_json(input_blueprint),
-				&read_as_json(input_blueprint_meta),
-				lossless
-			)?;
+			let entity = match version {
+				#[cfg(feature = "h1")]
+				GameVersion::H1 => Entity::from_game(
+					&read_as_json::<glacier_bin1::game::h1::STemplateEntity>(input_factory),
+					&read_as_json(input_factory_meta),
+					&read_as_json(input_blueprint),
+					&read_as_json(input_blueprint_meta),
+					lossless
+				)?,
+
+				#[cfg(feature = "h2")]
+				GameVersion::H2 => Entity::from_game(
+					&read_as_json::<glacier_bin1::game::h2::STemplateEntityFactory>(input_factory),
+					&read_as_json(input_factory_meta),
+					&read_as_json(input_blueprint),
+					&read_as_json(input_blueprint_meta),
+					lossless
+				)?,
+
+				#[cfg(feature = "h3")]
+				GameVersion::H3 => Entity::from_game(
+					&read_as_json::<glacier_bin1::game::h3::STemplateEntityFactory>(input_factory),
+					&read_as_json(input_factory_meta),
+					&read_as_json(input_blueprint),
+					&read_as_json(input_blueprint_meta),
+					lossless
+				)?,
+
+				#[cfg(feature = "fl")]
+				GameVersion::FL => Entity::from_game(
+					&read_as_json::<glacier_bin1::game::fl::STemplateEntityFactory>(input_factory),
+					&read_as_json(input_factory_meta),
+					&read_as_json(input_blueprint),
+					&read_as_json(input_blueprint_meta),
+					lossless
+				)?
+			};
 
 			fs::write(output, to_vec_float_format(&entity)).unwrap();
 		}
@@ -207,49 +236,56 @@ fn main() -> Result<()> {
 					output_factory_meta,
 					output_blueprint,
 					output_blueprint_meta,
-					h1
+					version
 				}
-		} => {
-			let (converted_fac, converted_fac_meta, converted_blu, converted_blu_meta) =
-				read_as_json::<Entity>(input).to_game()?;
+		} => match version {
+			#[cfg(feature = "h1")]
+			GameVersion::H1 => {
+				let (converted_fac, converted_fac_meta, converted_blu, converted_blu_meta) =
+					read_as_json::<Entity>(input).to_game::<(glacier_bin1::game::h1::STemplateEntity, _, _, _)>()?;
 
-			fs::write(output_factory, {
-				if h1 {
-					to_vec_float_format(&glacier_bin1::game::h1::STemplateEntity::convert_from(converted_fac).unwrap())
-				} else {
-					to_vec_float_format(&converted_fac)
-				}
-			})
-			.unwrap();
-
-			fs::write(output_factory_meta, to_vec_float_format(&converted_fac_meta)).unwrap();
-
-			fs::write(output_blueprint, {
-				if h1 {
-					to_vec_float_format(
-						&glacier_bin1::game::h1::STemplateEntityBlueprint::convert_from(converted_blu).unwrap()
-					)
-				} else {
-					to_vec_float_format(&converted_blu)
-				}
-			})
-			.unwrap();
-
-			fs::write(output_blueprint_meta, to_vec_float_format(&converted_blu_meta)).unwrap();
-		}
-
-		Command::Entity {
-			subcommand: EntityCommand::Normalise {
-				input,
-				output,
-				lossless
+				fs::write(output_factory, to_vec_float_format(&converted_fac)).unwrap();
+				fs::write(output_factory_meta, to_vec_float_format(&converted_fac_meta)).unwrap();
+				fs::write(output_blueprint, to_vec_float_format(&converted_blu)).unwrap();
+				fs::write(output_blueprint_meta, to_vec_float_format(&converted_blu_meta)).unwrap();
 			}
-		} => {
-			let (factory, factory_meta, blueprint, blueprint_meta) = read_as_json::<Entity>(input).to_game()?;
-			let entity = Entity::from_game(&factory, &factory_meta, &blueprint, &blueprint_meta, lossless)?;
 
-			fs::write(output, to_vec_float_format(&entity)).unwrap();
-		}
+			#[cfg(feature = "h2")]
+			GameVersion::H2 => {
+				let (converted_fac, converted_fac_meta, converted_blu, converted_blu_meta) =
+					read_as_json::<Entity>(input)
+						.to_game::<(glacier_bin1::game::h2::STemplateEntityFactory, _, _, _)>()?;
+
+				fs::write(output_factory, to_vec_float_format(&converted_fac)).unwrap();
+				fs::write(output_factory_meta, to_vec_float_format(&converted_fac_meta)).unwrap();
+				fs::write(output_blueprint, to_vec_float_format(&converted_blu)).unwrap();
+				fs::write(output_blueprint_meta, to_vec_float_format(&converted_blu_meta)).unwrap();
+			}
+
+			#[cfg(feature = "h3")]
+			GameVersion::H3 => {
+				let (converted_fac, converted_fac_meta, converted_blu, converted_blu_meta) =
+					read_as_json::<Entity>(input)
+						.to_game::<(glacier_bin1::game::h3::STemplateEntityFactory, _, _, _)>()?;
+
+				fs::write(output_factory, to_vec_float_format(&converted_fac)).unwrap();
+				fs::write(output_factory_meta, to_vec_float_format(&converted_fac_meta)).unwrap();
+				fs::write(output_blueprint, to_vec_float_format(&converted_blu)).unwrap();
+				fs::write(output_blueprint_meta, to_vec_float_format(&converted_blu_meta)).unwrap();
+			}
+
+			#[cfg(feature = "fl")]
+			GameVersion::FL => {
+				let (converted_fac, converted_fac_meta, converted_blu, converted_blu_meta) =
+					read_as_json::<Entity>(input)
+						.to_game::<(glacier_bin1::game::fl::STemplateEntityFactory, _, _, _)>()?;
+
+				fs::write(output_factory, to_vec_float_format(&converted_fac)).unwrap();
+				fs::write(output_factory_meta, to_vec_float_format(&converted_fac_meta)).unwrap();
+				fs::write(output_blueprint, to_vec_float_format(&converted_blu)).unwrap();
+				fs::write(output_blueprint_meta, to_vec_float_format(&converted_blu_meta)).unwrap();
+			}
+		},
 
 		Command::Patch {
 			subcommand: PatchCommand::Generate {
@@ -274,15 +310,13 @@ fn main() -> Result<()> {
 		}
 
 		Command::Patch {
-			subcommand:
-				PatchCommand::Apply {
-					input,
-					patch,
-					output,
-					permissive,
-					normalise,
-					format_fix
-				}
+			subcommand: PatchCommand::Apply {
+				input,
+				patch,
+				output,
+				permissive,
+				format_fix
+			}
 		} => {
 			let mut entity: Entity = read_as_json(input);
 			let mut patch: Patch = read_as_json(patch);
@@ -290,11 +324,6 @@ fn main() -> Result<()> {
 			if format_fix {
 				entity = from_slice(&to_vec_float_format(&entity))?;
 				patch = from_slice(&to_vec_float_format(&patch))?;
-			}
-
-			if normalise {
-				let (factory, factory_meta, blueprint, blueprint_meta) = entity.to_game()?;
-				entity = Entity::from_game(&factory, &factory_meta, &blueprint, &blueprint_meta, true)?;
 			}
 
 			let mut diagnostics_result = None;
@@ -308,11 +337,6 @@ fn main() -> Result<()> {
 			})?;
 
 			diagnostics_result.map_or(Ok(()), |e| Err(e))?;
-
-			if normalise {
-				let (factory, factory_meta, blueprint, blueprint_meta) = entity.to_game()?;
-				entity = Entity::from_game(&factory, &factory_meta, &blueprint, &blueprint_meta, true)?;
-			}
 
 			fs::write(output, to_vec_float_format(&entity)).unwrap();
 		}
